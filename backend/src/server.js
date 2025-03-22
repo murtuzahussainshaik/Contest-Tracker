0c0e67cb25d38d7a3f2975923467e5dc97eaca07
murtuzahussainshaik <murtuzahussainshaik@gmail.com>
2025-03-22 16:35:48 +0530

    moved server.js into src

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const contestRoutes = require("./routes/contestRoutes");
+const contestRoutes = require("../routes/contestRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
